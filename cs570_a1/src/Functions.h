e7021ca659eadc0fd7f952230b2cf31b4b94cdab
GIT Admin <ktb92154@gmail.com>
2013-06-02 00:00:11 -0400

    intial submit

---
+++ cs570_a1/src/Functions.h
@@ -0,0 +1,50 @@
+/* Keith Burgess,
+ * masc, masc
+ * CS570, Spring 2013
+ * Assignment #1:
+ * Filename: Functions.h
+ */
+
+#include<iostream>
+#include<string>
+#include<fstream>
+#include <pthread.h>
+#define NUM_THREADS	5
+
+using namespace std;
+
+// writing on a text file
+
+void *printFileInt(void *threadid) {
+	long tid;
+	tid = (long)threadid;
+	ofstream myfile;
+	myfile.open ("PROCTAB.txt");
+    myfile << threadid << " test\n";
+    myfile.close();
+}
+
+void *PrintHello(void *threadid)
+{
+   long tid;
+   tid = (long)threadid;
+   printf("Hello World! It's me, thread #%ld!\n", tid);
+   pthread_exit(NULL);
+}
+
+void *runThreads(){
+	pthread_t threads[NUM_THREADS];
+	int rc;
+	long t;
+
+	for(t=0;t<NUM_THREADS;t++){
+	     printf("In main process: creating thread %ld\n", t);
+
+	     rc = pthread_create(&threads[t], NULL, printFileInt, (void *)t);
+	     if (rc){
+	       printf("ERROR; return code from pthread_create() is %d\n", rc);
+
+	       }
+	     }
+	   pthread_exit(NULL);
+}
---
+++ cs570_a1/src/main.cpp
@@ -0,0 +1,31 @@
+/* Keith Burgess,
+ * masc, masc
+ * CS570, Spring 2013
+ * Assignment #1:
+ * Filename: assignment1.cpp
+ *
+ * Eclipse Notes:
+ * 1. properties->c/c++Build->setting->GCC C++ linker->libraries add "pthread" to top
+ */
+
+#include<iostream>
+#include<string>
+#include<fstream>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include"Functions.h"
+
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	pthread_t mainThread;
+	runThreads();
+
+   /* Last thing that main() should do */
+   pthread_exit(NULL);
+}
