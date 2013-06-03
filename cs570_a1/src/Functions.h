9f2814162a146282ed530139c13f345238f7983f
GIT Admin <ktb92154@gmail.com>
2013-06-03 01:36:25 -0400

    10 threads appending to file

--- cs570_a1/src/Functions.h
+++ cs570_a1/src/Functions.h
@@ -34,77 +34,72 @@ int createThreads(){
 	// initialize semaphore to 2b
     sem_init(&sem_lock, 0, 1);
 
-    pthread_t thread1, thread2, thread3;
+    pthread_t thread1, thread2, thread3, thread4, thread5, thread6, thread7, thread8, thread9 ,thread10;
     pthread_create(&thread1, NULL, &doSomething1, NULL);
-    pthread_create(&thread2, NULL, &doSomething2, NULL);
-    pthread_create(&thread3, NULL, &doSomething3, NULL);
+    pthread_create(&thread2, NULL, &doSomething1, NULL);
+    pthread_create(&thread3, NULL, &doSomething1, NULL);
+    pthread_create(&thread4, NULL, &doSomething1, NULL);
+    pthread_create(&thread5, NULL, &doSomething1, NULL);
+    pthread_create(&thread6, NULL, &doSomething1, NULL);
+    pthread_create(&thread7, NULL, &doSomething1, NULL);
+    pthread_create(&thread8, NULL, &doSomething1, NULL);
+    pthread_create(&thread9, NULL, &doSomething1, NULL);
+    pthread_create(&thread10, NULL, &doSomething1, NULL);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
     pthread_join(thread3, NULL);
+    pthread_join(thread4, NULL);
+    pthread_join(thread5, NULL);
+    pthread_join(thread6, NULL);
+    pthread_join(thread7, NULL);
+    pthread_join(thread8, NULL);
+    pthread_join(thread9, NULL);
+    pthread_join(thread10, NULL);
 
     return 0;
 }
 
-int sem_init(sem_t *sem, int pshared, unsigned int value);//initialize semaphore object
 
 // writing on a text file
 
 void printFileInt(unsigned int threadid) {
+	unsigned int tid = (unsigned int)pthread_self();
 	ofstream myfile;
-	myfile.open ("PROCTAB.txt", ios::out | ios::app );
-    myfile << threadid << " test\n";
+	//string filename = "PROCTAB.txt";
+	myfile.open("PROCTAB.txt", ios::out | ios::app );
+	cout << "Thread < " << tid << " > is writing to PROCTAB.txt\n";
+    myfile << threadid << "\n";
     myfile.close();
 }
 
+void something(unsigned int threadId) {
 
-
-void *doSomething(void*) {
-    // thread A
-	unsigned int tid = (unsigned int)pthread_self();
-    //printf("PID of this p: %u\n", tid);
-    return 0;
-}
-
-void something(char c) {
-
-    int i, time;
-    for (i = 0; i < 3; i++) {
-
+    int time;
+    	//cout << "Thread < " << threadId << " > is waiting\n";
         // P operation
         if (sem_wait(&sem_lock) == 0) {
 
             // generate random amount of time (< 30 seconds)
-            time = (int) ((double) rand() / RAND_MAX * 10);
-        	unsigned int tid = (unsigned int)pthread_self();
-        	printf("PID of this process: %u\n", tid);
-            printf("Thread %c enters and sleeps for %d seconds...\n", c, time);
-            printFileInt(tid);
+            time = (int) ((double) rand() / RAND_MAX * 4);
+            //cout << "Thread < " << threadId << " > has passed lock\n";
+            //printf("Thread %u enters and sleeps for %d seconds...\n", threadId, time);
+            printFileInt(threadId);
             sleep(time);
 
-            printf("Thread %c leaves the critical section\n", c);
+            //printf("Thread %u leaves the critical section\n", threadId);
 
             // V operation
             sem_post(&sem_lock);
         }
-    }
+    cout << "Thread < " << threadId << " > has left lock\n";
 }
 
 void *doSomething1(void*) {
     // thread A
-    something('A');
-    return 0;
-}
-
-void *doSomething2(void*) {
-    // thread B
-    something('B');
-    return 0;
-}
-
-void *doSomething3(void*) {
-    // thread C
-    something('C');
+	unsigned int tid = (unsigned int)pthread_self();
+	//cout << "Thread < " << tid << " > is running\n";
+    something(tid);
     return 0;
 }
 
